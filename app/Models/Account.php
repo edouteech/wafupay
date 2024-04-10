5aaa08731113d57add9b2181cc346c49775e79dc
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-04-10 18:49:49 +0200

    add account model and migrations

---
+++ app/Models/Account.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Account extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'account_num',
+        'balance',
+        'user_id',
+        'currency_id',
+        'is_verified'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function currency(): BelongsTo
+    {
+        return $this->belongsTo(Currency::class);
+    }
+}
