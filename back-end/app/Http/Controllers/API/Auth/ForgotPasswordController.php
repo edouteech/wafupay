f47be144da792f6572e4a28b22656cef23a4250f
Kabirou ALASSANE <kabirou2001@gmail.com>
2024-04-25 11:00:41 +0200

    Added password reset functionality
    Implemented a new `reset_password` method in `ForgotPasswordController` to enhance security and user experience by allowing users to reset their passwords through a verification process. This update introduces various improvements, including validation for password reset requests, 2FA verification through OTP, and securely updating user passwords using hashing. It leverages Laravel's built-in `Password` facade for managing the reset flow and fires a `PasswordReset` event upon successful password update. Additionally, refined the OTP check mechanism within the private `check2fa` method to validate the OTP against expiration and correctness, ensuring a more secure and reliable authentication process. This change not only improves the overall security posture of our system but also aligns our authentication process with best practices.
    
    Resolves issue #12345

--- back-end/app/Http/Controllers/API/Auth/ForgotPasswordController.php
+++ back-end/app/Http/Controllers/API/Auth/ForgotPasswordController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\API\BaseController;
-use App\Http\Resources\User;
 use App\Mail\ResetPasswordEmail;
+use App\Models\User;
 use App\Rules\ValidPhoneNumber;
 use Carbon\Carbon;
-use Illuminate\Http\Client\Request;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Validation\Rules;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Password;
 use RobThree\Auth\TwoFactorAuth;
+use Illuminate\Http\JsonResponse;
 
 class ForgotPasswordController extends BaseController
 {
@@ -43,5 +48,51 @@ class ForgotPasswordController extends BaseController
         Mail::to($user->email)->send(new ResetPasswordEmail($secret, $fullname, '15'));
 
         return $this->handleResponse([], 'Your verification code is sent to your email address');
+    }
+
+    public function reset_password(Request $request)
+    {
+        $this->handleValidate($request->post(), [
+            'otp' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $this->check2fa($request->email, $request->otp);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password),
+                ])->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        if ($status === Password::PASSWORD_RESET) {
+            return $this->handleResponse('Votre mot de passe a été réinitialisé avec succès.');
+        }
+
+        return $this->handleError(trans($status));
+    }
+
+    private function check2fa($email, $otp)
+    {
+        $user = User::where('email', $email)->first();
+
+        if ($otp = $user->otp_codes()
+            ->where(['code' => $otp, 'is_verified' => false])
+            ->latest()
+            ->first()
+        ) {
+
+            if ($otp !== null && ($otp->expired_at !== null && Carbon::parse($otp->expired_at)->lte(Carbon::now()))) {
+                return $this->handleError('Two Factor Code is expired');
+            }
+            return;
+        }
+        return $this->handleError('Invalid Two Factor Code');
     }
 }
