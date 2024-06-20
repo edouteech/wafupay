4ec4583698c45db37f71ebde38efbbed6c790f82
Charcrede <charcrede@gmail.com>
2024-06-20 18:25:56 +0100

    début du front et  création de la landing page

---
+++ front/tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors : {
+        "primary" : "#1877F2",
+        "secBlue" : "",
+        "textGray" : "#606060",
+        "thinBlue" : "#e3effd"
+
+      },
+      boxShadow : {
+        "shall" : "0px 4px 8px rgba(0, 0, 0, 1)"
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
