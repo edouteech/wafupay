e2dbf059b6f71a09394ac64100cbf5fe36d3c1ee
Ahmedable <ahmedabladon33@gmail.com>
2024-07-03 17:40:39 +0100

    update

--- front/src/app/(dashboard)/faq/page.tsx
+++ front/src/app/(dashboard)/faq/page.tsx
@@ -2,7 +2,8 @@
 import Dashbord from "../Components/Dashbord"
 import { useRouter } from "next/navigation"
 import { use, useState } from "react";
-import { ChevronDown, Eye, EyeOff } from "lucide-react";
+import { ChevronDown, ChevronUp, Eye, EyeOff } from "lucide-react";
+
 
 function Carte() {
     //################################## CONSTANTES #############################//
@@ -12,7 +13,11 @@ function Carte() {
     //################################## VARIABLES ##############################//
     const [showFaq, setShowFaq] = useState<boolean[]>([])
 
+    const [isOpen, setIsOpen] = useState(false);
 
+    const toggleAccordion = () => {
+        setIsOpen(!isOpen);
+    }
     //################################## MOUNTED ################################//
 
 
@@ -31,16 +36,28 @@ function Carte() {
     return (
         <>
             <Dashbord>
+
                 <div className="m-8">
                     <h2 className="text-center bg-white p-2 rounded-2xl">FAQ</h2>
                     <div className="p-4 bg-white my-3 rounded-2xl shadow-xl">
-                        <button className=" my-3font-bold text-black  rounded-2xl w-full flex justify-between items-center">
+                        <button
+                            onClick={toggleAccordion}
+                            className="my-3 font-bold text-black rounded-2xl w-full flex justify-between items-center"
+                        >
                             Comment puis-je envoyer de l'argent avec WAPUFAY ?
                             <div className="flex items-center justify-center h-8 w-8 bg-primary rounded-full">
-                                <ChevronDown className="text-white"></ChevronDown>
-                            </div> 
+                                {isOpen ? <ChevronUp className="text-white" /> : <ChevronDown className="text-white" />}
+                            </div>
                         </button>
-                        <p className="text-sm p-2">Pour envoyer de l'argent avec WAPUFAY, commencez par vous connecter à votre compte utilisateur. Ensuite, choisissez l'option d'envoi d'argent et saisissez les détails du destinataire, y compris son nom, son numéro de téléphone ou son adresse e-mail. Après avoir spécifié le montant que vous souhaitez envoyer, vous aurez l'opportunité de vérifier les informations de la transaction. Une fois que tout est correct, confirmez la transaction et vous recevrez une confirmation instantanée.</p>
+                        {isOpen && (
+                            <p className="text-sm p-2">
+                                Pour envoyer de l'argent avec WAPUFAY, commencez par vous connecter à votre compte utilisateur.
+                                Ensuite, choisissez l'option d'envoi d'argent et saisissez les détails du destinataire, y compris
+                                son nom, son numéro de téléphone ou son adresse e-mail. Après avoir spécifié le montant que vous
+                                souhaitez envoyer, vous aurez l'opportunité de vérifier les informations de la transaction. Une
+                                fois que tout est correct, confirmez la transaction et vous recevrez une confirmation instantanée.
+                            </p>
+                        )}
                     </div>
                 </div>
             </Dashbord>
--- front/src/app/(dashboard)/transactions/carte/page.tsx
+++ front/src/app/(dashboard)/transactions/carte/page.tsx
@@ -35,65 +35,67 @@ function Carte() {
     return (
         <>
             <Dashbord>
-                <div>
-                    <div className="text-base max-w-md mx-auto bg-white shadow-md rounded-lg p-6">
-                        <div className="flex justify-center space-x-4 mb-6 gap-4">
-                            <div className="flex items-center">
-                                <input type="radio" id="visa" className="w-6 h-6" name="card" />
-                                <label htmlFor="visa" className="pl-2"><Image alt="logo visa" src={visa}></Image></label>
-                            </div>
-                            <div className="flex items-center">
-                                <input type="radio" id="mscard" className="w-6 h-6" name="card" />
-                                <label htmlFor="mscard" className="pl-2"><Image alt="logo mscard" src={mscard}></Image></label>
-                            </div>
-                        </div>
-                        <div className="mb-6">
-                            <label className="block text-gray-700 font-semibold mb-2">De :</label>
-                            <div className="flex gap-4 flex-col px-8">
-                                <InputLabel type="text" id="num" label="Numéro de votre carte"></InputLabel>
-                                <InputLabel type="month" id="lavel" label="Date d'expiration"></InputLabel>
-                                <div className="relative">
-                                    <InputLabel type={`${showCvv ? 'text' : 'password'}`} id="lavel" label="CVV" classes="pl-32"></InputLabel>
-                                    <button onClick={() => { setShowCvv(!showCvv) }} className="z-10">
-                                        {showCvv && (
-                                            <EyeOff className="absolute top-4 right-5"></EyeOff>
-                                        )}
-                                        {!showCvv && (
-                                            <Eye className="absolute top-4 right-5"></Eye>
-                                        )}
-                                    </button>
+                <form action="">
+                    <div>
+                        <div className="text-base max-w-md mx-auto bg-white shadow-md rounded-lg p-6">
+                            <div className="flex justify-center space-x-4 mb-6 gap-4">
+                                <div className="flex items-center">
+                                    <input type="radio" id="visa" className="w-6 h-6" name="card" />
+                                    <label htmlFor="visa" className="pl-2"><Image alt="logo visa" src={visa}></Image></label>
+                                </div>
+                                <div className="flex items-center">
+                                    <input type="radio" id="mscard" className="w-6 h-6" name="card" />
+                                    <label htmlFor="mscard" className="pl-2"><Image alt="logo mscard" src={mscard}></Image></label>
                                 </div>
                             </div>
-                        </div>
-                        <div className="mb-6">
-                            <label className="block text-gray-700 font-semibold mb-2">Vers :</label>
-                            <div className="flex items-center mb-3">
-                                <img src="wave-logo.png" alt="Wave" className="w-10 h-10 mr-3" /> {/* Remplacez 'wave-logo.png' par le chemin correct */}
-                                <input
-                                    type="text"
-                                    placeholder="Montant"
-                                    className="w-full px-3 py-2 border border-gray-300 rounded-l focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                />
-                                <span className="bg-gray-100 border border-gray-300 p-2 rounded-r">FCFA</span>
-                            </div>
-                            <div className="flex items-center mb-3">
-                                <div className="flex items-center border border-gray-300 rounded-l p-2 bg-gray-100">
-                                    <img src="flag.png" alt="Flag" className="w-6 h-4 mr-2" /> {/* Remplacez 'flag.png' par le chemin correct */}
-                                    <span>+96</span>
+                            <div className="mb-6">
+                                <label className="block text-gray-700 font-semibold mb-2">De :</label>
+                                <div className="flex gap-4 flex-col px-8">
+                                    <InputLabel type="text" id="num" label="Numéro de votre carte"></InputLabel>
+                                    <InputLabel type="month" id="lavel" label="Date d'expiration"></InputLabel>
+                                    <div className="relative">
+                                        <InputLabel type={`${showCvv ? 'text' : 'password'}`} id="lavel" label="CVV" classes="pl-32"></InputLabel>
+                                        <button onClick={() => { setShowCvv(!showCvv) }} className="z-10">
+                                            {showCvv && (
+                                                <EyeOff className="absolute top-4 right-5"></EyeOff>
+                                            )}
+                                            {!showCvv && (
+                                                <Eye className="absolute top-4 right-5"></Eye>
+                                            )}
+                                        </button>
+                                    </div>
                                 </div>
-                                <input
-                                    type="text"
-                                    placeholder="Numéro de téléphone"
-                                    className="w-full px-3 py-2 border rounded-r focus:outline-none focus:ring-2 focus:ring-primary"
-                                />
                             </div>
+                            <div className="mb-6">
+                                <label className="block text-gray-700 font-semibold mb-2">Vers :</label>
+                                <div className="flex items-center mb-3">
+                                    <img src="wave-logo.png" alt="Wave" className="w-10 h-10 mr-3" /> {/* Remplacez 'wave-logo.png' par le chemin correct */}
+                                    <input
+                                        type="text"
+                                        placeholder="Montant"
+                                        className="w-full px-3 py-2 border border-gray-300 rounded-l focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                    />
+                                    <span className="bg-gray-100 border border-gray-300 p-2 rounded-r">FCFA</span>
+                                </div>
+                                <div className="flex items-center mb-3">
+                                    <div className="flex items-center border border-gray-300 rounded-l p-2 bg-gray-100">
+                                        <img src="flag.png" alt="Flag" className="w-6 h-4 mr-2" /> {/* Remplacez 'flag.png' par le chemin correct */}
+                                        <span>+96</span>
+                                    </div>
+                                    <input
+                                        type="text"
+                                        placeholder="Numéro de téléphone"
+                                        className="w-full px-3 py-2 border rounded-r focus:outline-none focus:ring-2 focus:ring-primary"
+                                    />
+                                </div>
+                            </div>
+                            <button className="w-full py-3 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition duration-300">
+                                Effectuer
+                            </button>
                         </div>
-                        <button className="w-full py-3 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition duration-300">
-                            Effectuer
-                        </button>
-                    </div>
 
-                </div>
+                    </div>
+                </form>
             </Dashbord>
 
 
